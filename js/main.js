c40572158c8331f25886d89daf9a9542644a67e8
Lucas <metallucas.ds@gmail.com>
2013-06-28 20:12:48 -0300

    Update js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-$("a[data-toggle=popover]").popover().click(function(e) {
-  e.preventDefault();
+jQuery(function() {
+  var isVisible = false;
+  var clickedAway = false;
+
+  $('.btn').popover().click(function(e) {
+    $(this).popover('show');
+
+    $('.closeb').click(function(e) {
+      $('.btn').popover('hide');
+    });
+    e.preventDefault();
+
+    $('.foo').click(function(e) {
+      alert('Yayow');
+    });
+  });
+
+  $(document).click(function(e) {
+    if ( isVisible & clickedAway ) {
+      $('.btn').popover('hide');
+      isVisible = clickedAway = false;
+    } else {
+      clickedAway = true;
+    }
+  });
 });
